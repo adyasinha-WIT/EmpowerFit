9f8f05d1848bee5af58e4d47561d491d67449945
Sarah Quinn <ssa.quinn@gmail.com>
2025-05-04 18:00:06 +1200

    Create ErrorViewModel

--- EmpowerFit/Areas/Basic/Controllers/HomeController.cs
+++ EmpowerFit/Areas/Basic/Controllers/HomeController.cs
@@ -1,12 +1,22 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+using ExFit.Models;
 
 namespace EmpowerFit.Areas.Basic.Controllers
 {
+    [Area("Basic")]
     public class HomeController : Controller
     {
         public IActionResult Index()
         {
             return View();
         }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
     }
+
 }
