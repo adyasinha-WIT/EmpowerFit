2689cd9236236fbdfd742b0c75e68b2a89f8acfa
Sarah Quinn <ssa.quinn@gmail.com>
2025-05-04 19:47:14 +1200

    Add iRepo & UoW

---
+++ EmpowerFit/Areas/Basic/Controllers/WorkoutController.cs
@@ -0,0 +1,23 @@
+﻿using ExFit.DataAcces.Repository.IRepository;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+
+
+namespace EmpowerFit.Areas.Basic.Controllers
+{
+    [Area("Basic")]
+    public class WorkoutController : Controller
+    {
+        private readonly IUnitOfWork _unitOfWork;
+        private readonly IWebHostEnvironment _webHostEnvironment;
+        public WorkoutController(IUnitOfWork unitOfWork, IWebHostEnvironment webHostEnvironment)
+        {
+            _unitOfWork = unitOfWork;
+            _webHostEnvironment = webHostEnvironment;
+        }
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
