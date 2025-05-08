8b5a308ef25c4621754f6cc2bfd68a37c9d0e7bc
AdyaS <118127954+adyasinha-WIT@users.noreply.github.com>
2025-05-09 01:40:48 +1200

    Error- trying to add login/register

--- ExFit.DataAcces/Data/ApplicationDbContext.cs
+++ ExFit.DataAcces/Data/ApplicationDbContext.cs
@@ -4,7 +4,7 @@ using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 
 namespace ExFit.DataAcces.Data
 {
-    public class ApplicationDbContext : DbContext
+    public class ApplicationDbContext : IdentityDbContext
     {
         public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
             : base(options)
