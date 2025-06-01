d28e39a01d082f4f7a737de75251a5b5e3ed3e4a
Sarah Quinn <ssa.quinn@gmail.com>
2025-06-01 21:31:37 +1200

    updated data annotations

--- ExFit.Models/TemporaryLocationModel.cs
+++ ExFit.Models/TemporaryLocationModel.cs
@@ -1,8 +1,11 @@
-﻿namespace Exfit.Models;
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Exfit.Models;
 
 public class TemporaryLocationModel
 {
     public double Latitude { get; set; }
     public double Longitude { get; set; }
+    [Required]
     public string UserId { get; set; }
 }
