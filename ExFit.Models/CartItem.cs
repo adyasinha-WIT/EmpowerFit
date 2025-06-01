a891f75675f6d8bc31c2bfb75f3e7d20592882aa
Sarah Quinn <ssa.quinn@gmail.com>
2025-06-01 20:00:30 +1200

    data annotations added to models

--- ExFit.Models/CartItem.cs
+++ ExFit.Models/CartItem.cs
@@ -1,14 +1,18 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+
 
 namespace ExFit.Models
 {
     public class CartItem
     {
         public int Id { get; set; }
+
+        [Required(ErrorMessage = "Membership Plan is required.")]
         public int MembershipPlanId { get; set; }
         public MembershipPlan MembershipPlan { get; set; }
         public int CartId { get; set; }
--- ExFit.Models/Workout.cs
+++ ExFit.Models/Workout.cs
@@ -1,6 +1,7 @@
 ﻿using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -10,13 +11,18 @@ namespace ExFit.Models
     public class Workout
     {
         public int Id { get; set; }
+        [StringLength(500, ErrorMessage = "Goals cannot exceed 500 characters.")]
         public string Goals { get; set; }
+        [StringLength(500, ErrorMessage = "Goals cannot exceed 500 characters.")]
         public string Workouts { get; set; }
+        [StringLength(500, ErrorMessage = "Goals cannot exceed 500 characters.")]
 
         public string Type { get; set; }
+        [StringLength(500, ErrorMessage = "Goals cannot exceed 500 characters.")]
 
         public string WeeklyReport { get; set; }
 
+        [Url(ErrorMessage = "Media URL must be a valid URL.")]
         public string? MediaUrl { get; set; } // For image/video path
     }
 }
